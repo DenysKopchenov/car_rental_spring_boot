be0b0520e9d63657897c40a4486ab52bf6a01608
Denys Kopchenov <denisko555@gmail.com>
2022-10-16 15:21:48 +0300

    Navbar, Header, Footer
    -change html for fragments, add logo and bg

--- src/main/java/com/dkop/car/rental/dto/CarDto.java
+++ src/main/java/com/dkop/car/rental/dto/CarDto.java
@@ -1,7 +1,9 @@
 package com.dkop.car.rental.dto;
 
 import com.dkop.car.rental.model.car.CategoryClass;
+import com.dkop.car.rental.model.car.Fuel;
 import com.dkop.car.rental.model.car.Manufacturer;
+import com.dkop.car.rental.model.car.Transmission;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +23,8 @@ public class CarDto {
     private UUID id;
     private Manufacturer manufacturer;
     private CategoryClass categoryClass;
+    private Fuel fuel;
+    private Transmission transmission;
     private String model;
     private long pricePerDay;
     private MultipartFile image;
