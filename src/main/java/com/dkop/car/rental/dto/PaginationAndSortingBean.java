3130e474c613c722ea431b4dbae0da5a93646111
Denys Kopchenov <denisko555@gmail.com>
2022-10-19 16:14:47 +0300

    Orders view
    - change orders pages for user and manager

--- src/main/java/com/dkop/car/rental/dto/PaginationAndSortingBean.java
+++ src/main/java/com/dkop/car/rental/dto/PaginationAndSortingBean.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 public class PaginationAndSortingBean {
 
     private Integer page = 1;
-    private Integer size = 5;
+    private Integer size = 4;
     private String sort;
     private String direction = "ASC";
 }
