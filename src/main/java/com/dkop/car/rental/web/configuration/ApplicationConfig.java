e086d501ff8b9eb5cc5e983649ff8bdfdad4a1ce
Denys Kopchenov <denisko555@gmail.com>
2022-10-29 18:33:20 +0300

    Heroku
    - deploy on heroku

--- src/main/java/com/dkop/car/rental/web/configuration/ApplicationConfig.java
+++ src/main/java/com/dkop/car/rental/web/configuration/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package com.dkop.car.rental.web.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,16 +9,31 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.client.RestTemplate;
 
+import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
 public class ApplicationConfig {
+
+    @Value("${MAIL_SENDER_USERNAME}")
+    private String mailSenderUserName;
+    @Value("${MAIL_SENDER_PASSWORD}")
+    private String mailSenderPassword;
+    @Value("${POSTGRES_URL}")
+    private String dataSourceUrl;
+    @Value("${DB_USERNAME}")
+    private String dataSourceUsername;
+    @Value("${DB_PASSWORD}")
+    private String dataSourcePassword;
 
     @Bean
     public JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
+
+        mailSender.setUsername(mailSenderUserName);
+        mailSender.setPassword(mailSenderPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
@@ -26,6 +43,16 @@ public class ApplicationConfig {
         props.put("mail.debug", "true");
 
         return mailSender;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+
+        dataSourceBuilder.url(dataSourceUrl);
+        dataSourceBuilder.username(dataSourceUsername);
+        dataSourceBuilder.password(dataSourcePassword);
+        return dataSourceBuilder.build();
     }
 
     @Bean
